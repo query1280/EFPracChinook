2ff4610f84dada5a637ef7dbcab178022fe5c931
query1280 <41275409+query1280@users.noreply.github.com>
2018-11-05 17:17:14 +0900

    Practice sstart

---
+++ ChinookPrac_1105/ChinookPrac_1105/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookPrac_1105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
